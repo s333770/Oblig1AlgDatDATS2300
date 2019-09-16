5a2f28ff9dff111210d646a8339ac4c3abe3eda8
Laila2903 <laila_doudouh@hotmail.com>
2019-09-16 14:07:06 +0200

    opgave 1

--- src/Oblig1/Oblig1.java
+++ src/Oblig1/Oblig1.java
@@ -1,5 +1,77 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
 public class Oblig1 {
+
+    public static int[] randPermBoolean(int n){ //Kopiert fra boken for generering av testverdier
+        Random r= new Random();
+        int []a =new int[n];
+        boolean [] har = new boolean[n];
+
+        for(int i=0; i<n;){
+            int k=r.nextInt(n);
+            if(har[k]==false){
+                har[k]=true;
+                a[i++]=k+1;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+        return a;
+    }
+
+    public static void bytt(int[] a, int i, int j)
+    {
+        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
+
+    public static int maks (int [] a){
+
+        if (a.length < 1) {     //sjekker at tabellen ikke er tom
+            throw new NoSuchElementException("Tabellen er tom!");
+        }
+
+        int max = a[0]; // Initierer variabel med størst verdi
+
+        for (int i = 0; i< a.length-1; i++) {  //Bruker bobblestortering til å sammenlikne to og to tall ved siden av hverandre
+
+            if (a[i] > a[i+1]) {
+                bytt(a,a[i],a[i+1]);
+                max = a [i+1];
+            }
+        }
+        return max;
+    }
+
+    public static int ombyttinger (int [] a){
+        if (a.length < 1){     //sjekker at tabellen ikke er tom
+            throw new NoSuchElementException("Tabellen er tom!");
+        }
+
+        int teller = 0; //instansierer telleren
+
+        int max = a[0]; // Initierer variabel med størst verdi
+
+        for (int i = 0; i< a.length-1; i++) {  //Bruker bobblestortering til å sammenlikne to og to tall ved siden av hverandre
+
+            if (a[i] > a[i+1]) {
+                bytt(a,a[i],a[i+1]);
+                teller++;
+                max = a [i+1];
+            }
+            else {
+                max = a[i+1];
+            }
+        }
+        return teller;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+       int [] a=  randPermBoolean(10);
+       maks(a);
+       
+
+
     }
 }
